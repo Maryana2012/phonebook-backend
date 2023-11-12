948d80c2808e0a5664e148c2714c79e7f2910499
Maryana2012 <114877890+Maryana2012@users.noreply.github.com>
2023-11-12 16:36:00 +0200

    controller signup

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,20 +1,33 @@
 import User from '../db/models/userModel.js';
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+dotenv.config();
+
+const {SECRET_KEY} = process.env;
 
 const signup = async (req, res, next) => {
    const {name, email, password}=req.body;
+  
    try {
-      const user=User.findOne(email);
-      if( user){
-         res.status(409).json({message: 'Email in use'})
+      const user=await User.findOne({email});
+      
+      if(user){
+         res.status(409).json({message: 'Email in use'});
+         return
+
       }
-      // const newUser = User.create(name, email, password);
-      // res.status(201).json(newUser)
       const newUser = new User({name, email, password});
       await  newUser.hashPassword(password);
       await  newUser.save();
+      const payload = {id: newUser._id};
+
+      const token = jwt.sign(payload, SECRET_KEY);
+   
+      await User.findByIdAndUpdate(newUser._id,{token})
+      
       res.status(201).json(newUser)
    } catch (error) {
-    console.log(error)
+   //  console.log(error)
     req.status(500).json({message: error.message})
    } 
 }
--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,10 @@ import express from 'express';
 import controllers from '../controllers/users-controllers.js'
 const usersRouter = express.Router();
 
-usersRouter.post('/users/signup', controllers.signup);
+usersRouter.post('/signup', controllers.signup);
 // usersRouter.post('/users/login');
 // usersRouter.post('/users/logout');
 // usersRouter.get('/users/current');
+
 
 export default usersRouter;
